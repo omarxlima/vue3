562daec1582d46b5cd6ea18230ad0102f890083b
marxlima <marxmaciel@gmail.com>
2024-04-05 18:07:43 -0300

    eventos modificadores de acesso

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,23 @@
 <template>
   <Topo></Topo>
 
-  <!-- <div v-show="showName">
-    Nome: {{ name }}
-    SobreNome: {{ lastName }}
-  </div>
-
-  <div v-if="accessLevel == 'admin'">Admin</div>
-  <div v-else-if="accessLevel == 'marketing'"> Marketin</div>
-  <div v-else>User</div> -->
-  <!-- <h1 :class="classH1"> Curso de vue3 </h1> 
-  <h1 :class="styleClass"> Curso de vue3 </h1>
-
-  <p :class="['text', {'title-home': isHome}]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit sint consequatur deleniti 
-    oluptas! Quos nihil dicta laudantium, accusamus, rem doloremque labore nostrum obcaecati impedit 
-    quidem qui quam perspiciatis facilis quis.</p>
-
-    <p :style="{ 'color': 'aqua', 'background-color': 'black' }" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit nisi earum vel, dolor perferendis optio eaque quidem accusamus
-       porro magni nobis molestias ad vitae! Repudiandae incidunt obcaecati in necessitatibus numquam.</p>
-
-
-
   <div>
-    <div v-for="(todo, index) in todos" :key="todo.id">
-      <img v-if="todo.imgSrc" :src="todo.imgSrc">
-      {{ index }} - {{ todo.title }}
-    </div>
+    <!-- @ no logar da diretiva v-on  -->
+    <button @click.once="onClick">
+      Enviar
+    </button>
 
   </div>
 
-
-
-  <HelloWorld />
--->
-
-<div>
-  <div>
-    One-way data binding
-
-    Two-way data binding
-    v-model -> formulários
-    <br>
-    <br>
-
-   <div>
-    <label for="nome"> Nome:</label> <br>
-    <input type="text" v-model="nome">
-    <br>
-    {{ nome }}
-
-    <br>
-    <br>
-    <label for="">Estados:</label> <br>
-    <select v-model="estados">
-        <option value="">Selecione</option>
-        <option value="Maranhão">Maranhão</option>
-        <option value="Rio de Janeiro">Rio de Janeiro</option>
-        <option value="São Paulo">São Paulo</option>
-
-    </select> <br> <br>
-
-    {{  estados }}
-
-    <div>
-      <label for=""> newsLetter</label>
-      <input  v-model="newsletter" type="radio" value="Sim"> Sim 
-      <input  v-model="newsletter" type="radio" value="Não"> Não
-      <br> <br>
-
-      {{ newsletter }}
-
-      <br> <br>
-
-      
-      
-      
-      
-    </div>
-    
-    <div>
-
-      <label for="">Contratos</label>
-        <input v-model="contrato" type="checkbox"> Aceita nossos termos... <br>
-
-        {{ contrato }}
-    </div>
-
-   </div>
-
-   <div>
-    <label for=""> Selecione as cores</label>
-      <input  v-model="colors" type="checkbox" value="Azul"> Azul 
-      <input  v-model="colors" type="checkbox" value="Vermelho"> Vermelho
-      <input  v-model="colors" type="checkbox" value="Preto"> Preto
-
-      <br> <br>
-
-      {{ colors }}
-   </div>
-
-
-    <!-- <br> -->
-
-    <!-- {{ nome }} -->
+  <div @mouseover="onMouseover" @mouseout="onMouseOut">
+    Mouse over
 
   </div>
-</div>
 
-
-
+  <form action="https://google.com" @submit.prevent="onSubmit">
+      <input type="text" @keyup.enter="onKeyUp">
+      <button type="submit"> Enviar</button>
+  </form>
 
 
 </template>
@@ -121,68 +30,40 @@ import Topo from './components/Topo.vue'
 export default {
   name: 'App',
   components: {
-  //   /* HelloWorld,
     Topo
   },
   data() {
     return {
-      nome : 'Karl Marx',
-      estados: '',
-      newsletter: 'Sim',
-      contrato: true,
-      colors: [],
-      // showHeader: true,
-      // name: 'Luke',
-      // lastName: 'Skywalker',
-      // showName: true,
-      // accessLevel: 'admin',
-      todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false,
-          "imgSrc": 'https://placehold.co/400'
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false,
-          "imgSrc": 'https://placehold.co/400'
 
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
-        }
-      ],
-      isHome : true,
-      classH1:'title',
-      pClass : 'text',
-      styleClass: { 'color': 'aqua', backGroundColor: 'black' },
-     
 
+
+    }
+  },
+
+  methods: {
+    onClick($event) {
+      console.log('click', $event)
+    },
+
+    onMouseover($event) {
+      console.log('mouse over', $event)
+    },
+
+    onMouseOut($event) {
+      console.log('mouse out', $event)
+    },
+    onSubmit() {
+      console.log('submit')
+    },
+    onKeyUp($event) {
+      console.log('keyup', $event)
     }
 
 
-    
 
-    
+
+
+
   }
 }
 
@@ -198,17 +79,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} 
+}
 
 
 .title {
   font-size: 20px;
   color: blue;
 }
+
 .title-home {
   font-size: 40px;
   color: green;
 }
+
 .text {
   color: blueviolet;
 }
